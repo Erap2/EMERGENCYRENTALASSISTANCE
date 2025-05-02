7777512b60dc6e372b9029c6560676efab2bcb57
Erap2 <fredrichardberg0090@gmail.com>
2025-05-02 12:46:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("applyButton").addEventListener("click", function () {
+  window.open("https://forms.zohopublic.com/housingemergencyassistancesgm1/form/SignUpRegistrationForm/formperma/GvCIJlJkTsMKDYzK_2neDVpBHcvtpEuY7kCByn4q8mc", "_blank");
+});
